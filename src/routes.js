ec5c94a9a113d9812f3dcd8b75910bb70b8ec2e2
Priscilla <prsc.ribeiro@gmail.com>
2019-03-10 22:48:40 -0300

    Use require-dir to import the controllers in the routes

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,12 @@ const express = require('express')
 
 const routes = express.Router()
 const authMiddleware = require('./app/middlewares/auth')
-const UserController = require('./app/controllers/UserController')
-const SessionController = require('./app/controllers/SessionController')
 
-routes.post('/users', UserController.store)
-routes.post('/sessions', SessionController.store)
+// Import require-dir
+const controllers = require('./app/controllers')
+
+routes.post('/users', controllers.UserController.store)
+routes.post('/sessions', controllers.SessionController.store)
 
 routes.get('/teste', authMiddleware, (req, res) => {
   res.json({ ok: true })
